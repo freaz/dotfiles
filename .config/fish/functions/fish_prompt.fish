5c9a33dc243f98bc5d993977d866d95ae6a456fa
Ondřej Musil <omusil@gmail.com>
2020-01-18 23:42:14 +0000

    fish: yello prompt on ssh sessions

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,7 +2,7 @@ function fish_prompt
   if not test $status -eq 0
     set_color $fish_color_error
   else if test -n "$SSH_TTY"
-    set_color FF69B4
+    set_color FFFF00
   else
     set_color $fish_color_cwd
   end
