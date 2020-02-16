ed44b0c13061cd5a0f9622864c7ca14521a4eed1
Ondřej Musil <omusil@gmail.com>
2020-02-16 19:27:54 +0100

    fish: add gwip and gunwip aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,6 +19,8 @@ alias s=subl
 alias gpg-ls='gpg --list-secret-keys --keyid-format LONG'
 alias pretty-json='python -m json.tool'
 alias vim-plug-update='curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+alias gwip='git add -A; git rm (git ls-files --deleted) 2> /dev/null; git commit --no-verify --no-gpg-sign -m "--wip-- [skip ci]"'
+alias gunwip='git log -n 1 | grep -q -c "\-\-wip\-\-" && git reset HEAD~1'
 
 # Docker Aliases
 alias dm='docker-machine'
